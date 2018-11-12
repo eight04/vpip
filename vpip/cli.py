1e6fddb3705d1192526f241a5747436d0ec0890e
eight <eight04@gmail.com>
2018-11-13 02:39:56 +0800

    Add: command fallback

--- vpip/cli.py
+++ vpip/cli.py
@@ -1,21 +1,38 @@
 import argparse
 import sys
+import shlex
+from configparser import ConfigParser
+from pathlib import Path
 
 from . import commands
 
 def cli():
+    # set_subparser_fallback(subparsers, fallback)
+    patch_argparse()
+        
     parser = argparse.ArgumentParser(prog="vpip")
     args = sys.argv[1:]
     
-    # redirect unknown command to `run ...`?
-    # if args and args[0] not in commands.names and not args[0].startswith("-"):
-        # args.insert(0, "run")
+    def fallback(values):
+        config = ConfigParser()
+        file = Path("setup.cfg")
+        try:
+            config.read_string(file.read_text(encoding="utf8"))
+            command = config.get("vpip", "command_fallback", fallback=None)
+            if command is not None:
+                return ["run", *shlex.split(command), *values]
+        except OSError:
+            pass
+            
+    subparsers = parser.add_subparsers(
+        title="subcommands", dest="COMMAND", required=True, metavar="COMMAND",
+        fallback=fallback)
         
-    subparsers = parser.add_subparsers(title="subcommands", dest="COMMAND", required=True, metavar="COMMAND")
     modules = commands.get_modules()
     for name, module in modules.items():
         command = subparsers.add_parser(name, help=module.help)
         add_arguments(command, getattr(module, "options"))
+        
     ns, extra = parser.parse_known_args(args)
     
     module = modules[ns.COMMAND]
@@ -38,3 +55,23 @@ def add_arguments(target, options):
             if not isinstance(name, list):
                 name = [name]
             target.add_argument(*name, **option)
+
+def patch_argparse():
+    # pylint: disable=protected-access
+    __init__ = argparse._SubParsersAction.__init__
+    def init(self, *args, **kwargs):
+        self._fallback = kwargs.pop("fallback", None)
+        __init__(self, *args, **kwargs)
+        if self._fallback:
+            self.choices = None
+    argparse._SubParsersAction.__init__ = init
+    
+    __call__ = argparse._SubParsersAction.__call__
+    def call(self, *args, **kwargs):
+        values = args[2]
+        if values[0] not in self._name_parser_map and self._fallback:
+            new_values = self._fallback(values)
+            if new_values is not None:
+                values[:] = new_values
+        return __call__(self, *args, **kwargs)
+    argparse._SubParsersAction.__call__ = call
