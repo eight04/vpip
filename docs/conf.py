d31a8c3bda0f37dd8397e98cbff46997dd7f2914
eight <eight04@gmail.com>
2018-11-16 16:40:13 +0800

    Add: more information to doc

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@
 import os.path
 import sys
 
+import vpip
+
 # sys.path.insert(0, os.path.realpath(__file__ + "/../.."))
 # add_module_names = False
 extensions = ["sphinx.ext.intersphinx", "sphinx.ext.autodoc"]
@@ -11,3 +13,8 @@ intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 autoclass_content = 'both'
 # exclude_patterns = ['api/vpip.rst']
 autodoc_inherit_docstrings = False
+
+project = "vpip"
+author = "eight04"
+copyright = "2018, eight04"
+version = vpip.__version__
