fb1eb425a137dd5136a639eb9d97bf56a2e2bde0
eight <eight04@gmail.com>
2019-02-10 15:44:24 +0800

    Add: pytest

--- cute.py
+++ cute.py
@@ -9,7 +9,7 @@ def readme():
     
 cute(
     pkg_name = "vpip",
-    test = ['pylint {pkg_name} cute.py', 'readme_build'],
+    test = ['pylint {pkg_name} cute.py test.py', 'pytest test.py', 'readme_build'],
     bump_pre = 'test',
     bump_post = ['dist', 'release', 'publish', 'install'],
     dist = 'x-clean build dist && python setup.py sdist bdist_wheel',
