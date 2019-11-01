12533e3b9cbbcea9d1a817942f1454a8aa0f4a7e
eight <eight04@gmail.com>
2019-11-01 15:49:58 +0800

    Add: update_venv command (#20)
    * Update readme
    
    * Update pyxcute
    
    * Add: update_venv command
    
    * Update doc
    
    * Fix: it seems that there is no _base_executable in older python

---
+++ docs/api/vpip.commands.update_venv.rst
@@ -0,0 +1,9 @@
+vpip.commands.update_venv
+=========================
+
+``update_venv`` command.
+
+.. automodule:: vpip.commands.update_venv
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ vpip/commands/update_venv.py
@@ -0,0 +1,55 @@
+help = "Update/rebuild the venv"
+options = [
+    {
+        "name": ["-g", "--global"],
+        "nargs": "*",
+        "dest": "global_",
+        "help": "Update global packages",
+        "metavar": "PACKAGE"
+    }
+]
+
+def run(ns):
+    from .. import venv
+    if ns.global_ is None:
+        update_venv(venv.get_current_venv())
+        return
+        
+    if not ns.global_:
+        ns.global_ = venv.iter_global_packages()
+        
+    for pkg in ns.global_:
+        update_venv(venv.get_global_pkg_venv(pkg), global_pkg_name=pkg)
+    
+def update_venv(vv, global_pkg_name=None):
+    """Update a venv.
+    
+    :arg vpip.venv.Venv vv: A venv instance.
+    :arg str global_pkg_name: Decide how to rebuild the venv. If set then run :func:`vpip.commands.install.install_global` when rebuilding venv. Otherwise, run :func:`vpip.commands.install.install_local_first_time`
+    
+    If the Python version is upgraded, this command reinstall the entire venv.
+    """
+    import sys
+    import pathlib
+    import configparser
+    env_dir = pathlib.Path(vv.env_dir)
+    config = configparser.ConfigParser()
+    config.read_string("[DEFAULT]\n" + (env_dir / "pyvenv.cfg").read_text())
+    config_home = pathlib.Path(config.get("DEFAULT", "home"))
+    # https://github.com/python/cpython/blob/0118d109d54bf75c99a8b0fa9aeae1a478ac4b7e/Lib/venv/__init__.py#L109
+    current_home = pathlib.Path(getattr(sys, '_base_executable', sys.executable)).parent
+    if config_home != current_home:
+        # python is updated, rebuild the entire venv
+        vv.destroy()
+        from . import install
+        if global_pkg_name:
+            install.install_global([global_pkg_name])
+        else:
+            install.install_local_first_time()
+        return
+    
+    # update pip
+    from .. import pip_api
+    with vv.activate():
+        pip_api.install("pip", upgrade=True, latest=True)
+    
