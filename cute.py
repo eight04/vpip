2f13140f1eae2c27f8cff56117ad43c3d6195da6
eight <eight04@gmail.com>
2019-11-01 14:42:14 +0800

    Update pyxcute

--- cute.py
+++ cute.py
@@ -1,23 +1,5 @@
-from xcute import cute, f
+from xcute import cute, LiveReload
 
-def readme():
-    """Live reload readme"""
-    from livereload import Server
-    import asyncio
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    server = Server()
-    server.watch("README.rst", "python cute.py readme_build")
-    server.serve(open_url_delay=1, root="build/readme")
-    
-def doc():
-    """Live reload docs"""
-    from livereload import Server
-    import asyncio
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    server = Server()
-    server.watch(f("{pkg_name}"), "python cute.py doc_build")
-    server.serve(open_url_delay=1, root="docs/build")
-    
 cute(
     pkg_name = "vpip",
     test = ['pylint {pkg_name} cute.py', 'pytest', 'readme_build'],
@@ -40,10 +22,10 @@ cute(
          'build/readme/index.rst build/readme/index.html')
     ],
     readme_pre = "readme_build",
-    readme = readme,
-    doc_build = "sphinx-build docs docs/build",
+    readme = LiveReload("README.rst", "readme_build", "build/readme"),
+    doc_build = "sphinx-build docs build/docs",
     doc_pre = "doc_build",
-    doc = doc,
+    doc = LiveReload(["{pkg_name}", "docs"], "doc_build", "build/docs"),
     # I guess it is not a good idea to generate this automatically...
     doc_api = [
         "sphinx-apidoc vpip --no-toc --separate -o docs/api",
